1d8009efaf3de6c63aa6c4cced1d7cc7b9f4142c
AnhDieuTran <100272354+AnhDieuTran@users.noreply.github.com>
2023-09-30 12:54:12 +0700

    update

--- client/src/components/Emotion.js
+++ client/src/components/Emotion.js
@@ -1,9 +1,54 @@
-import React from 'react'
+const Emotion =() =>{
+  const handleItemClick = (selectedTopic) => {
+      // Xử lý khi người dùng nhấp vào ô
+      console.log(`Bạn đã nhấp vào mục: ${selectedTopic.name}`);
+      // Thêm xử lý tùy ý tại đây
+    };
 
-const Emotion = () => {
-  return (
-    <div>Emotion</div>
+  const list = [
+      {
+          name: '',
+          imageUrl: '',
+          
+      },
+      {
+          name: '',
+          imageUrl: '',
+      },
+      {
+          name: '',
+          imageUrl: '',
+      },
+      {
+          name: '',
+          imageUrl: '',
+      },
+  ]
+  return(
+      <div className="p-10 grid grid-cols-4 gap-10 md:gap-6">
+          {list.map((oneOfEmotion, index) => (
+          <div
+              key={index}
+              className="border p-2 rounded-xl outline outline-1 cursor-pointer transition-transform hover:scale-105" 
+              onClick={() => handleItemClick(oneOfEmotion)} // Gọi hàm xử lý khi người dùng nhấp vào ô
+          >
+              <div
+              className="flex h-72"
+              style={{
+                  backgroundImage: `url(${oneOfEmotion.imageUrl})`,
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                  backgroundRepeat: 'no-repeat',
+              }}
+              ></div>
+              <div className="text-lg font-semibold mt-2 text-center">
+              {oneOfEmotion.name}
+              </div>
+          </div>
+          ))}
+      </div>
+
   )
 }
 
-export default Emotion
+export default Emotion;
--- client/src/components/Family.js
+++ client/src/components/Family.js
@@ -1,9 +1,54 @@
-import React from 'react'
+const Family =() =>{
+  const handleItemClick = (selectedTopic) => {
+      // Xử lý khi người dùng nhấp vào ô
+      console.log(`Bạn đã nhấp vào mục: ${selectedTopic.name}`);
+      // Thêm xử lý tùy ý tại đây
+    };
 
-const Family = () => {
-  return (
-    <div>Family</div>
+  const list = [
+      {
+          name: '',
+          imageUrl: '',
+          
+      },
+      {
+          name: '',
+          imageUrl: '',
+      },
+      {
+          name: '',
+          imageUrl: '',
+      },
+      {
+          name: '',
+          imageUrl: '',
+      },
+  ]
+  return(
+      <div className="p-10 grid grid-cols-4 gap-10 md:gap-6">
+          {list.map((oneOfFamily, index) => (
+          <div
+              key={index}
+              className="border p-2 rounded-xl outline outline-1 cursor-pointer transition-transform hover:scale-105" 
+              onClick={() => handleItemClick(oneOfFamily)} // Gọi hàm xử lý khi người dùng nhấp vào ô
+          >
+              <div
+              className="flex h-72"
+              style={{
+                  backgroundImage: `url(${oneOfFamily.imageUrl})`,
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                  backgroundRepeat: 'no-repeat',
+              }}
+              ></div>
+              <div className="text-lg font-semibold mt-2 text-center">
+              {oneOfFamily.name}
+              </div>
+          </div>
+          ))}
+      </div>
+
   )
 }
 
-export default Family
+export default Family;
